f492b3ca981b99876de39cc62497a5f0dc5beee1
hyunsunny3 <47172698+hyunsunny3@users.noreply.github.com>
2019-02-24 15:54:34 +0900

    ok

--- portfolio/test/js/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.skill-icons').children('.active').each(function(i) {
-    var row = $(this);
-    setTimeout(function() {
-      row.css('background','#c0392b');
-    }, 100*i);
-  });
-});
